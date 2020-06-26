a5b283d835ff1c2d062579ae5d6672e71459ab39
fedgut <fedgut@gmail.com>
2020-06-26 11:34:29 -0500

    remove comment

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
-/* eslint-disable no-unused-vars */
 import React from 'react';
-import PropTypes from 'prop-types';
-import Big from 'big.js';
+// import PropTypes from 'prop-types';
+// import Big from 'big.js';
 import '../css/App.css';
 
 function App() {
