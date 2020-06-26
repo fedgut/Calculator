ee393aebb0d5b254220edea6fd27fb0af109de94
fedgut <fedgut@gmail.com>
2020-06-26 11:23:27 -0500

    reinstall linters

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/index.css';
 import App from './components/App';
+import './css/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
