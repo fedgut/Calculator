7c9a911ee077e0d234dd7cd7f1e7e8f80648c9c5
fedgut <fedgut@gmail.com>
2020-06-26 12:15:48 -0500

    delete stickler config, droping stickler in favour of github actions

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,8 @@ import '../css/App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-      </header>
+      <Display />
+      <Button />
     </div>
   );
 }
