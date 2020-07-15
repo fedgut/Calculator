4fee589c178d5d227eacdc4693e90beee9601922
fedgut <fedgut@gmail.com>
2020-07-15 15:31:22 -0500

    Create calculation.js and operate.js

---
+++ src/logic/calculate.js
@@ -0,0 +1,3 @@
+export default function calculate(data, buttonName) {
+  const { total, next, operation } = data;
+}
---
+++ src/logic/operate.js
@@ -0,0 +1,5 @@
+import Big from 'big.js';
+
+export default function operate(numberOne, numberTwo, operation) {
+  
+}
