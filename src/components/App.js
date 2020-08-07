65127152bda131a15117b7de43d2437734b65835
fedgut <fedgut@gmail.com>
2020-08-07 11:07:15 -0500

    crete new branch for development of event handlers

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ButtonPanel from './buttonPanel';
 import Display from './display';
 import '../css/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Display />
-      <ButtonPanel />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() = {
+  this.state({
+      
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Display />
+        <ButtonPanel />
+      </div>
+    );
+  }
 }
 
 export default App;
