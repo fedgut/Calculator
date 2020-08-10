9d31bacf8d3e91ee5414277ad26e43cd84a61271
fedgut <fedgut@gmail.com>
2020-08-10 15:22:53 -0500

    Add handleClick  and pass props to display and ButtonPanel on main component

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ButtonPanel from './buttonPanel';
 import Display from './display';
 import '../css/App.css';
+import calculate from '../logic/calculate';
 
 class App extends Component {
   constructor() {
@@ -14,17 +15,22 @@ class App extends Component {
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick() = {
-  this.state({
-      
-    })
+  handleClick(buttonName) {
+    const { total, next, operation } = calculate(this.state, buttonName);
+    this.setState({
+      total,
+      next,
+      operation,
+    });
   }
 
   render() {
+    const { next, total } = this.state;
+    const show = next || total || '0';
     return (
       <div className="App">
-        <Display />
-        <ButtonPanel />
+        <Display show={show} />
+        <ButtonPanel onClick={this.handleClick} />
       </div>
     );
   }
